05e6ca00d3f05311d89c9fb0cd4cb38b140d8a57
zhangyf <429594730@qq.com>
2019-08-13 00:37:28 +0800

    #

---
+++ src/view/auth-service-dlg.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <el-form label-width="80">
+      <el-form-item label="用户"></el-form-item>
+      <el-form-item label="IP限制"></el-form-item>
+      <el-form-item label="范围限制"></el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    edit: Boolean
+  },
+  data() {},
+  methods() {}
+};
+</script>
+
+<style>
+</style>
