2a827e71ea28ae1421550bbc211f0d61376c5550
zhangyanfei <429594730@qq.com>
2019-08-12 08:59:20 +0800

    #

--- src/view/auth-service.vue
+++ src/view/auth-service.vue
@@ -9,24 +9,25 @@
         <el-form-item label="范围"></el-form-item>
       </el-form>
     </el-dialog>
-    <el-table :data="authList">
+    <el-table :data="authList" border>
       <el-table-column type="index" label="序号" width="60" header-align="center"></el-table-column>
       <el-table-column prop="userName" label="授权用户" width="150"></el-table-column>
-      <el-table-column prop="ip" label="IP" width="150"></el-table-column>
+      <el-table-column prop="ip" label="IP限制" width="150"></el-table-column>
       <el-table-column prop="token" label="令牌"></el-table-column>
       <el-table-column prop="updateAt" label="更新时间" width="160" header-align="center"></el-table-column>
-      <el-table-column label="范围" width="80" align="center" header-align="center">
+      <el-table-column label="范围限制" width="80" align="center" header-align="center">
         <template slot-scope="scope">
-          <el-button :disabled="!!scope.row.extent" size="mini">查看</el-button>
+          <el-tag v-if="scope.row.extent" size="mini" type="danger">受限</el-tag>
+          <el-tag v-else size="mini" type="success">不限</el-tag>
         </template>
       </el-table-column>
 
-      <!-- <el-table-column width="100" fixed="right">
-        <div slot-scope="{row}">
-          <el-button size="mini">详情</el-button>
-          <el-button size="mini" type="danger">删除</el-button>
+      <el-table-column width="150" fixed="right" label="操作" align="center" >
+        <div slot-scope="{scope}">
+          <el-button size="mini" @click="editAuth(scope.row)">修改</el-button>
+          <el-button size="mini" type="danger" @click="deleteAuth(scope.row)">删除</el-button>
         </div>
-      </el-table-column> -->
+      </el-table-column>
     </el-table>
   </div>
 </template>
@@ -44,6 +45,14 @@ export default {
     api.serviceAuth.list().then(data => {
       this.authList = data.list;
     });
+  },
+  methods: {
+    editAuth(auth) {
+      debugger;
+    },
+    deleteAuth(auth) {
+      debugger;
+    }
   }
 };
 </script>
